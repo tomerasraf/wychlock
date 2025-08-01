805d127907eefb141f5b8ac90a92d0c3e9c92bdb
EytanShur <wer.eik@gmail.com>
2025-08-01 16:45:59 +0300

    asd

--- Assets/Scripts/Player/Movment.cs
+++ Assets/Scripts/Player/Movment.cs
@@ -7,7 +7,7 @@ public class Movment : MonoBehaviour
 {
     [SerializeField] private float speed = 3.0f;
     [SerializeField] private Transform body;
-    private Vector2 rotationDir = Vector2.zero;
+    private Vector2 rotationDir;
     public Vector2 inputDirection;
     public Vector3 mousePos;
 
@@ -17,22 +17,14 @@ public class Movment : MonoBehaviour
         transform.Translate(inputDirection * speed * Time.deltaTime);
     }
 
-    private static Vector2 PlayerMovement()
-    {
-        Vector2 inputDirection;
-        float xAxis = Input.GetAxis("Horizontal");
-        float yAxis = Input.GetAxis("Vertical");
-
-        inputDirection = new Vector2(xAxis, yAxis).normalized;
-        return inputDirection;
-    }
+   
 
     public void RotateToMousePos(Vector3 mousePos)
     {
-        
-        Vector2 direction = (mousePos - transform.position).normalized;
+
+        rotationDir = (mousePos - transform.position).normalized;
        
-        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float angle = Mathf.Atan2(rotationDir.y, rotationDir.x) * Mathf.Rad2Deg;
        
         body.rotation = Quaternion.Euler(0f, 0f, angle + 180);
     }
