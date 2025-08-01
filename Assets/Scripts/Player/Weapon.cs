78a408e880eb5142146213393586f335ede15cad
EytanShur <wer.eik@gmail.com>
2025-08-01 19:23:16 +0300

    saf

--- Assets/Scripts/Player/Weapon.cs
+++ Assets/Scripts/Player/Weapon.cs
@@ -9,21 +9,6 @@ public enum WeaponTypes
     Sniper,
     GrenadeLauncher
 }
-[CreateAssetMenu(fileName = "NewWeapon", menuName = "Weapons")]
-public class WeaponType : ScriptableObject
-{
-    public WeaponTypes weaponType;
-    [Tooltip("Time between Shots")]
-    public float fireRate;
-    public int bulletPerShot;
-    public float spreadDeegres;
-    public float recoilForce;
-    public GameObject bulletPrefab;
-    public float bulletSpeed;
-    public int bulletDamage;
-    public float lifeSpan;
-
-}
 public class Weapon : MonoBehaviour
 {
     [SerializeField] WeaponType weaponType;
