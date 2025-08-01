e82fa151aa4f84f62e66371b35e3f4962c1709cb
KerenHavro <keren11222332@gmail.com>
2025-08-01 16:59:24 +0300

    Starting to do healthbar

--- Assets/Scripts/Player/Health.cs
+++ Assets/Scripts/Player/Health.cs
@@ -2,20 +2,25 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour
 {
     public int hp {get; private set;}
     [SerializeField] int maxHp;
     public static Action playerDeath;
+    public Slider healthSlider;
 
     private void Start()
     {
+        healthSlider.maxValue = maxHp;
+        healthSlider.value = hp;
         Heal(maxHp);
     }
     public void Damage(int damage)
     {
         hp -= damage;
+        healthSlider.value=hp;
 
         if (hp < 0)
         {
