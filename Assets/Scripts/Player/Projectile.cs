cb26faac6bc046f375cfbd27e6cc7a49c70a1707
EytanShur <wer.eik@gmail.com>
2025-08-02 10:13:19 +0300

    asd

--- Assets/Scripts/Player/Projectile.cs
+++ Assets/Scripts/Player/Projectile.cs
@@ -10,19 +10,25 @@ public class Projectile : MonoBehaviour
     private Vector2 direction;
     private int damage = 20;
     private float lifeTime = 2f;
+    private Sprite sprite;
+
+    private SpriteRenderer spriteRenderer;
 
     private float lifeTimer = 0;
 
-    public void Init(float speed, Vector2 direction, int damage, float lifeTime)
+    public void Init(float speed, Vector2 direction, int damage, float lifeTime, Sprite sprite)
     {
         this.speed = speed;
         this.direction = direction;
         this.damage = damage;
         this.lifeTime = lifeTime;
+        this.sprite = sprite;
     }
     private void OnEnable()
     {
         lifeTimer = 0;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = sprite;
     }
     private void Update()
     {
